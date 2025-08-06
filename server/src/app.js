633b6931d67e47f63ebb3934bf6389d179aa5ded
piyush4001 <156915493+piyush4001@users.noreply.github.com>
2025-08-06 23:58:29 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -8,14 +8,12 @@ import { errorHandler } from "./middlewares/error.middleware.js"
 
 const app = express()
 
-
-// CORS
 app.use(
   cors({
-    origin: ["https://yojanas-for-u.vercel.app", "http://localhost:5173"],
+    origin: process.env.CORS_ORIGIN,
     credentials: true,
   })
-)
+);
 
 app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
