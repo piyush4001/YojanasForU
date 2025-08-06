3a3ec2147eb66a9a2302f8b6b3ca1c43510c96c2
piyush4001 <156915493+piyush4001@users.noreply.github.com>
2025-08-07 00:09:53 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -8,12 +8,17 @@ import { errorHandler } from "./middlewares/error.middleware.js"
 
 const app = express()
 
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: process.env.CORS_ORIGIN,
+//     credentials: true,
+//   })
+// );
+app.use(cors({
+  origin: "*",
+  credentials: true,
+}));
+
 
 app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
