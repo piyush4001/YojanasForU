a088eb022d411c0a0f9f6d439a7343641e564f0e
piyush4001 <156915493+piyush4001@users.noreply.github.com>
2025-08-06 21:06:08 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -6,7 +6,7 @@ import { errorHandler } from "./middlewares/error.middleware.js"
 const app = express()
 app.use(
   cors({
-    origin: "*",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 )
